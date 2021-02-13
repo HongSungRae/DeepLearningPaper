53b4cccaa7d4f5c68262aa62c18d2f3505f00707
Hong <sr5043@naver.com>
2021-02-14 00:37:46 +0900

    seft

--- Codes/SetFunctionForTimeSeries/dataprocess/physionet2012.py
+++ Codes/SetFunctionForTimeSeries/dataprocess/physionet2012.py
@@ -1,10 +1,12 @@
 import pandas as pd
 import os
+import time
 
 root = '/daintlab/data/sr/paper/setfunction/tensorflow_datasets/root/tensorflow_datasets/downloads/extracted/'
 A_root = root + 'A'
 B_root = root + 'B'
 C_root = root + 'C'
+
 
 
 def get_length():
@@ -18,18 +20,40 @@ def get_length():
         len(next(os.walk(B_root+'/set-b'))[2]),
         len(next(os.walk(C_root+'/set-c'))[2])))
 
-def txt_to_csv(target_folder):
-    txts = next(os.walk(target_folder))[2]
+
+
+def txt_to_csv(folder,target):
+    target = pd.read_csv(target)
+    general_features = ["RecordID","Age","Gender","Height","ICUType","Weight"]
+    features = ["Weight", "ALP", "ALT", "AST", "Albumin", "BUN", "Bilirubin", "Cholesterol",
+                "Creatinine", "DiasABP", "FiO2", "GCS", "Glucose", "HCO3", "HCT", "HR", "K",
+                "Lactate", "MAP", "MechVent", "Mg", "NIDiasABP", "NIMAP", "NISysABP", "Na",
+                "PaCO2", "PaO2", "Platelets", "RespRate", "SaO2", "SysABP", "Temp", "TroponinI",
+                "TroponinT", "Urine", "WBC", "pH"]
+
+    txts = next(os.walk(folder))[2]
     length = len(txts)
     for i in range(length):
-        print(txts[i])
-        a = pd.read_csv(target_folder+txts[i])
-    print('done')    
+        data = pd.read_csv(folder+txts[i])
+        ID = data.iloc[0,2]
+        #print(txts[i])
+        #for j in range(6): # 일부 애들은 00:00에 너무 많은게 조사됨.
+        for k in range(6,len(data)):
+            time = list(map(int,data.iloc[k,0].split(':')))
+            s_i = ((time[0]*60+time[1])*0.1,
+                    data.iloc[k,2],
+                    features.index(data.iloc[k,1]))
+            print(s_i)
+            break
 
-    #os.walk(target_folder)
+        # 처음 012345 는 기본정보 -> 논문에서는 안 쓴것으로 보임
+        # col0 : Time, col1 : Parameter, col3 : Value
+        # Time은 HH:MM 꼴이지만 어느순간부터 HH:MM:00 이런식으로 나오는 애들이 있음
+        # set을 어떻게 저장? List? tensor? dataframe?
+    else:
+        print('=== Done ! ===')
 
 
 if __name__=='__main__':
-    get_length()
-
-    txt_to_csv(A_root+'/set-a/')
+    #get_length()
+    txt_to_csv(A_root+'/set-a/',A_root+'/Outcomes-a.csv')
