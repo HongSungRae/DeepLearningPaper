5464f6a7164d4166d9861f4e29b97a167244c4c5
Hong <sr5043@naver.com>
2021-02-25 13:26:41 +0900

    seft

--- Codes/SetFunctionForTimeSeries/models/transformer.py
+++ Codes/SetFunctionForTimeSeries/models/transformer.py
@@ -10,35 +10,74 @@ class Transformer(nn.Module):
     def __init__(self,d_embed=3,d_k=128,seq_len=500,h1=2,h2=2,h3=2,N1=4,N2=4):
         super().__init__()
         self.encoder = Encoder(d_embed,d_k,seq_len,h1,N1)
-        self.decoder = Decoder(h2,h3,N2)
+        self.decoder = Decoder(d_embed,d_k,seq_len,h2,h3,N2)
 
     def forward(self,x,n):
-        x = self.encoder(x,n)
-        x = self.decoder(x)
-        return x
+        c = self.encoder(x,n)
+        output = self.decoder(c,n,x)
+        return output
 
 
 
 
 class Decoder(nn.Module):
-    def __init__(self,h2,h3,N2):
+    def __init__(self,d_embed,d_k,seq_len,h2,h3,N2):
         super().__init__()
-        pass
+        self.N2 = N2
+        self.list_masked_multiheadattn = []
+        self.list_decoder_multiheadattn = []
+        self.list_feedforward = []
+        self.fc1 = nn.Linear(seq_len*d_embed,1024)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(1024,1)
+        self.sigmoid = nn.Sigmoid()
+        
+        for i in range(self.N2):
+            self.masked_multiheadattn = Masked_MultiHeadAttn(d_embed,d_k,seq_len,h2)
+            self.decoder_multiheadattn = DecoderMultiHeadAttn(d_embed,d_k,seq_len,h3)
+            self.feedforward = FeedForward(d_embed,d_k,seq_len,h3)
+            self.list_masked_multiheadattn.append(self.masked_multiheadattn)
+            self.list_decoder_multiheadattn.append(self.decoder_multiheadattn)
+            self.list_feedforward.append(self.feedforward)
     
-    def forward(self,x):
-        return None
+    def forward(self,c,n,x):
+        for i in range(self.N2):
+            output = self.list_masked_multiheadattn[i](x,n)
+            output = self.list_decoder_multiheadattn[i](output,n,c)
+            output = self.list_feedforward[i](output)
+        output = output.reshape(x.shape[0],-1)
+        output = self.fc1(output)
+        output = self.relu(output)
+        output = self.fc2(output)
+        output = self.sigmoid(output) # 0 < x < 1
+        return x
 
 
-
-'''
-class Generator(nn.Module):
-    def __init__(self):
+class DecoderMultiHeadAttn(nn.Module):
+    def __init__(self,d_embed,d_k,seq_len,h1,decoding=False):
         super().__init__()
-        pass
+        self.sdpattn = SDPAttn(d_embed,d_k,seq_len,h1,decoding)
+        self.d_model = d_k*h1
+        self.fc = FCLayer(self.d_model,d_embed)
+        self.layer_norm = nn.LayerNorm([seq_len,d_embed])
+
+    def forward(self,x,n,c): # residual and normalization
+        outcome = self.sdpattn(x,n)
+        outcome = self.fc(outcome)
+        outcome = outcome + x
+        outcome = self.layer_norm(outcome)
+        return outcome
+
+
+
+class Masked_MultiHeadAttn(nn.Module):
+    def __init__(self,d_embed,d_k,seq_len,h1):
+        super().__init__()
+        self.masked_multiheadattn = MultiHeadAttn(d_embed,d_k,seq_len,h1,decoding=True)
     
-    def forward(self,x):
-        return None
-'''
+    def forward(self,x,n):
+        outcome = self.masked_multiheadattn(x,n)
+        return outcome
 
 
 
@@ -86,9 +125,9 @@ class FeedForward(nn.Module):
 
 
 class MultiHeadAttn(nn.Module):
-    def __init__(self,d_embed,d_k,seq_len,h1):
+    def __init__(self,d_embed,d_k,seq_len,h1,decoding=False):
         super().__init__()
-        self.sdpattn = SDPAttn(d_embed,d_k,seq_len,h1)
+        self.sdpattn = SDPAttn(d_embed,d_k,seq_len,h1,decoding)
         self.d_model = d_k*h1
         self.fc = FCLayer(self.d_model,d_embed)
         self.layer_norm = nn.LayerNorm([seq_len,d_embed])
@@ -104,23 +143,32 @@ class MultiHeadAttn(nn.Module):
 
 
 class SDPAttn(nn.Module):
-    def __init__(self,d_embed,d_k,seq_len,h1):
+    def __init__(self,d_embed,d_k,seq_len,h1,decoding):
         super().__init__()
         self.d_k = d_k
         self.d_model = d_k*h1
+        self.decoding = decoding
         self.q_layer = FCLayer(d_embed,self.d_model)
         self.k_layer = FCLayer(d_embed,self.d_model)
         self.v_layer = FCLayer(d_embed,self.d_model)
         self.pad_masking = PadMasking(seq_len)
+        self.sub_masking = SubMasking(seq_len)
         self.softmax = nn.Softmax(dim=-1)
     
-    def forward(self,x,n):
-        Q = self.q_layer(x)
-        K = self.k_layer(x)
-        V = self.v_layer(x)
+    def forward(self,x,n,c=None,**kargs):
+        if c == None:
+            Q = self.q_layer(x)
+            K = self.k_layer(x)
+            V = self.v_layer(x)
+        else:
+            Q = self.q_layer(c)
+            K = self.k_layer(c)
+            V = self.v_layer(x)
         outcome = torch.matmul(Q,torch.transpose(K,-1,-2))
         outcome = outcome/np.sqrt(self.d_k)
         outcome = self.pad_masking(outcome,n)
+        if self.decoding == True:
+            outcome = self.sub_masking(outcome,n)
         outcome = self.softmax(outcome)
         outcome = torch.matmul(outcome,V)
         return outcome
@@ -146,6 +194,22 @@ class PadMasking(nn.Module):
             return self.mask_matrix
 
 
+class SubMasking(nn.Module):
+    def __init__(self,seq_len=500):
+        super().__init__()
+        self.seq_len = seq_len
+
+    def forward(self,x,n):
+        if len(x.shape) == 3:
+            for i in range(x.shape[0]):
+                for j in range(n-1):
+                    x[i,j,j+1:] = -10e+8
+        else:
+            for j in range(n-1):
+                x[j,j+1:] = -10e+8
+        
+        return x
+
 
 
 class FCLayer(nn.Module):
@@ -170,4 +234,8 @@ if __name__=='__main__':
 
     encoder = Encoder(d_embed=3,d_k=128,seq_len=500,h1=2,N1=4)
     context = encoder(x,n)
-    print(context.shape)
+    print('contxt :' ,context.shape)
+
+    transformer = Transformer()
+    y_pred = transformer(x,n)
+    print('y_pred :',y_pred.shape)
