8ef90e517dfc4042a586c81594591fde293548b8
Hong <sr5043@naver.com>
2021-03-10 15:01:08 +0900

    seft

--- Codes/SetFunctionForTimeSeries/models/seft.py
+++ Codes/SetFunctionForTimeSeries/models/seft.py
@@ -20,10 +20,15 @@ class CLASS_(nn.Module):
 
 
 class SeFT(nn.Module):
-    def __init__(self,m=4):
+    def __init__(self,m=4,d=128):
         super().__init__()
+        self.m = m
+        self.d = d
         self.set_function = SetFunc()
-        self.attention = Attention()
+        self.attention1 = Attention()
+        self.attention2 = Attention()
+        self.attention3 = Attention()
+        self.attention4 = Attention()
         self.fc1 = nn.Linear(128*m,256)
         self.relu1 = nn.ReLU()
         self.dropout = nn.Dropout(p=0.5)
@@ -33,12 +38,24 @@ class SeFT(nn.Module):
         self.sigmoid = nn.Sigmoid()
     
     def forward(self,x,n):
+        bs = x.shape[0]
+        ###
+        is_cuda = torch.cuda.is_available()
+        device = torch.device('cuda' if is_cuda else 'cpu')
+        query = torch.zeros(bs,self.m,self.d)
+        if is_cuda:
+            query = query.float().to(device)
+        ###
         ### original input x is used to make Q for attention mechanism ###
         # ▼ encoding process
         e = self.set_function(x,n) # shape : (bs,2)
-        e = self.attention(e,n,x)
+        r_1 = self.attention1(e,n,x,query)
+        r_2 = self.attention2(e,n,x,query)
+        r_3 = self.attention3(e,n,x,query)
+        r_4 = self.attention4(e,n,x,query)
+        r_asterisk = torch.cat((r_1,r_2,r_3,r_4),dim=1)
         # ▼ decoding process
-        e = e.view(e.shape[0],-1)
+        e = r_asterisk.view(r_asterisk.shape[0],-1)
         e = self.fc1(e)
         e = self.relu1(e)
         e = self.dropout(e)
@@ -105,9 +122,48 @@ class G(nn.Module):
         return x
 
 
+class Attention(nn.Module):
+    def __init__(self,d=128):
+        super().__init__()
+        self.d = d
+        self.h = H()
+        self.softmax = nn.Softmax(dim=1)
 
+    def forward(self,f_S,n,x,query):
+        #####
+        is_cuda = torch.cuda.is_available()
+        device = torch.device('cuda' if is_cuda else 'cpu')
+        #####
 
+        bs = x.shape[0]
+        n_list = n.tolist()
 
+        matrix = torch.zeros(bs,1024,5)
+        matrix[0:,0:,0:2] = f_S.view(bs,1,2)
+        matrix[0:,0:,2:] = x
+        weight = torch.randn(5,self.d)
+        r_i = torch.zeros(64,128)
+        if is_cuda:
+            matrix = matrix.float().to(device)
+            weight = weight.float().to(device)
+            r_i = r_i.float().to(device)
+        key = torch.matmul(matrix,weight)
+        e_ji = torch.sum(key[0:,0:,0:]*query[0:,0,0:].view(64,1,128),2).view(bs,1024,1)/np.sqrt(self.d)
+        for j in range(bs): # Masking
+            length = int(n_list[j][0])
+            e_ji[j,length:,0] = -1000.0
+        a_ji = self.softmax(e_ji) # [64,1024,1]
+        h_s = self.h(x) # [64,128]
+        print(a_ji.shape,h_s.shape)
+            
+        # r_i 연산하기
+        for k in range(bs):
+            temp = torch.sum(a_ji[k]*h_s[k],dim=0)
+            r_i[k] = temp
+
+        return r_i
+
+""" class Attention above this is more clear and works well
 class Attention(nn.Module):
     def __init__(self,m=4,d=128):
         super().__init__()
@@ -162,7 +218,7 @@ class Attention(nn.Module):
         for l in range(self.m-1):
             concat_r = torch.cat((concat_r,self.r_asterisk[l+1]),dim=1)
         return concat_r
-
+"""
 
 
 
